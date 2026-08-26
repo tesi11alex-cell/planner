fa3f16aa24ff0917c1598657f940facdf0d14225
tesi11alex-cell <tesi11alex@gmail.com>
2026-08-26 14:16:44 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'planner-offline-';
-const CACHE_NAME = 'planner-offline-v14-note-settimanali';
+const CACHE_NAME = 'planner-offline-v16-note-altro-divisi';
 
 const APP_SHELL = [
   './',
