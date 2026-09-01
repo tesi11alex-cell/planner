a72086f36596ec9a6d2856b61c3cee45d1b91af5
tesi11alex-cell <tesi11alex@gmail.com>
2026-09-01 12:21:02 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_PREFIX = 'planner-offline-';
-const CACHE_NAME = 'planner-offline-v29-orari-2h';
+const CACHE_NAME = 'planner-offline-v30-vista-standard-linea';
 
 const APP_SHELL = [
   './',
   './index.html',
   './Agenda.html',
   './manifest-v10.webmanifest',
-  './planner-v29.css',
-  './planner-v29.js',
+  './planner-v30.css',
+  './planner-v30.js',
   './agenda-icon-180-v10.png',
   './agenda-icon-192-v10.png',
   './agenda-icon-512-v10.png'
