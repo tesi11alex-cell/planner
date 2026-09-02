a0433115e1b6dd98fdffc4b8b23a00667c91ab7f
tesi11alex-cell <tesi11alex@gmail.com>
2026-09-02 07:22:56 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_PREFIX = 'planner-offline-';
-const CACHE_NAME = 'planner-offline-v32-meccanismo-unico';
+const CACHE_NAME = 'planner-offline-v33-note-come-giorno';
 
 const APP_SHELL = [
   './',
   './index.html',
   './Agenda.html',
   './manifest-v10.webmanifest',
-  './planner-v32.css',
-  './planner-v32.js',
+  './planner-v33.css',
+  './planner-v33.js',
   './agenda-icon-180-v10.png',
   './agenda-icon-192-v10.png',
   './agenda-icon-512-v10.png'
